e7a337df8710f1db1c137bef2384ed65f1563492
Harry Merzin <harry@milosets.me>
2017-05-13 18:33:57 -0400

    center text field stuff

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import TextField from 'material-ui/TextField';
+import FlatButton from 'material-ui/FlatButton';
 
 export default class Search extends Component {
     constructor(props) {
         super();
     }
 
+    style = {
+        display: 'flex',
+        justifyContent: 'center',
+
+    }
+
     render() {
         return (
-            <TextField floatingLabelText="Floating Label Text" />            
+
+            <div style={this.style}>
+                <TextField floatingLabelText="Floating Label Text" /> 
+                <FlatButton style={{marginTop: 30, marginLeft: 15}} label="Submit" primary={true} />
+            </div>
+                       
         )
     }
 }
