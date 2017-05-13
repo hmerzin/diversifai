7faabe09d7b8b614f2799c003fff83102dccf153
Kaveh Khorram <kavehkhorram@Kavehs-MacBook-Pro.local>
2017-05-13 16:57:37 -0400

    structs

---
+++ src/components/Search.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Search extends Component {
+    constructor(props) {
+        super();
+    }
+
+    render() {
+        return (
+            <h2>Search</h2>
+        )
+    }
+}
