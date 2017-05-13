0fdc91fff3f8bbc46df830b804cfb2042780001a
Rick Bross <rbross3@gmail.com>
2017-05-13 17:17:54 -0400

    add api route

--- server/index.js
+++ server/index.js
@@ -5,6 +5,13 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.post('/api', function (req, res) {
+  var link = req.param('url');
+  res.send({
+    link
+  });
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
